855a5ee7932b17bfc074e032759baa43ef6f8665
rxspidera <Rx.spidera@outlook.com>
2025-11-02 17:48:06 +0600

    Create Namespaces_void.cpp

---
+++ Namespaces_void.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Learning Topic 2: Namespaces & Void Functions..
+// Fun exercise: 2 characters using namespaces
+
+
+namespace dumbo1 {
+string name = "X";
+int age = 16;
+string gender = "Female";
+string worth = "unidentified";
+
+void introduction() {  
+		cout << "Hey there, Im here to cook." << '\n' << "cook my life." << '\n' << "Nah wait..🌪️im here to be cooked!!" <<'\n';  
+	}  
+}  
+  
+namespace dumbo2 {  
+	string name = "Y";  
+	int age = 16;  
+	string gender = "Male";  
+	string worth = "-9999999";  
+	  
+	void introduction() {  
+		cout << "Im chill.." << '\n' << " Just  a normal human being🥂" <<'\n';  
+	}  
+}
+
+int main() {
+cout << dumbo1:: name << '\n';
+dumbo1::introduction();
+
+cout << dumbo2::name <<'\n';  
+	dumbo2::introduction();  
+	  
+  
+return 0;
+
+}
