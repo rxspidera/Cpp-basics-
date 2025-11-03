8e22a8c721ed3bfe79226ceaacc43f358c7fc1e6
rxspidera <Rx.spidera@outlook.com>
2025-11-03 23:38:49 +0600

    Create Type_aliases.cpp

---
+++ Type_aliases.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Learning topic 3 : type aliases..
+// Totally serious C++ program🧊
+
+using dog = string;
+using me = string;
+using banana = string;
+
+int main() {
+    dog work = "Dog is debugging reality🦮";
+    me odd_habit = "Reading every damn i see on roads..cz why not🍺";
+    banana status = "Confused if its a fruit or variable🍌";
+
+    cout << "What's dog so busy with?" << endl << work << endl;
+    cout << "Share one of your habit: " << odd_habit << endl;
+    cout << "Banana status: " << status << endl;
+
+    cout << "\n Everything is fine. Absolutely fine. 🌀" << endl;
+    return 0;
+}
