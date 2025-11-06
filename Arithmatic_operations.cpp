9810fe9c9f9222ce1f76032c53cd7e833701a602
rxspidera <Rx.spidera@outlook.com>
2025-11-06 23:02:43 +0600

    Create Arithmatic_operations.cpp

---
+++ Arithmatic_operations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Learning topic 5: Simple arithmatic operations in c++
+//And..yeahh im alive 😌 still functioning with 2 brain cells
+
+int main() {
+   
+    int a = 10, b = 3;
+    
+    cout << "a = 10" <<  ", " << "b = 3" <<endl;
+    // Addition
+    cout << "a + b = " << (a + b) << endl;
+    // Subtraction
+    cout << "a - b = " << (a - b) << endl;
+    // Multiplication
+    cout << "a * b = " << (a * b) << endl;
+    // Division
+    cout << "a / b = " << (a / b) << endl;  
+    // Integer division
+    cout << "a % b = " << (a % b) << endl;
+
+    return 0;
+}
