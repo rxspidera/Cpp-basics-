4eae68c76bd0eb204c497b0ec857ad45dc26c658
rxspidera <Rx.spidera@outlook.com>
2025-11-13 00:07:08 +0600

    Create cmath.cpp

---
+++ cmath.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// Learning topic 8: Some arithmetic operations in C++
+
+int main() {
+    double x = 4.0;
+    double y = 3.0;
+    double z = 56.78;
+    
+    cout << "x = 4.0" << endl << "y = 3.0" << endl << "z= 56.78" << endl << endl;
+    
+    cout << "Power (x,y): " << pow(x, y) << endl;
+    cout << "Sqrt (x): " << sqrt(x) << endl;
+    cout << "Abs(-7): " << abs(-7) << endl;
+    cout << "Ceil(z): " << ceil(z) << endl;
+    cout << "Floor(z): " << floor(z) << endl;
+    cout << "Round(z): " << round(z) << endl;
+    
+    return 0;
+}
